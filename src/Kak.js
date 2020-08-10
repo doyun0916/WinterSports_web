6c3b97799afba172d6a921e6ce11697499c87019
doyun0916 <doyun0916@naver.com>
2020-08-10 23:23:59 +0900

    kakaotalk_channel function added

---
+++ src/Kak.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Kak() {
+    return (
+      <div>
+        <div
+            id="kakao-talk-channel-chat-button"
+            data-channel-public-id="_PatIK"
+            data-title="consult"
+            data-size="small"
+            data-color="yellow"
+            data-shape="pc"
+            data-support-multiple-densities="true"
+        />
+     </div>
+    )
+}
+
+export default Kak;
